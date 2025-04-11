fd8c616fd5f62392f3c143d626e13ae2c61238e2
Yasar Arslan <s2415981@edu.savonia.fi>
2025-04-11 10:34:09 +0300

    MokitPage päivitetty

---
+++ Mokkivaraus/Mokki.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokkivaraus
+{
+    public class Mokki
+    {
+        public string Nimi { get; set; }
+        public string Sijainti { get; set; }
+        public decimal Hinta { get; set; }
+        public int Kapasiteetti { get; set; }
+        public string Alue { get; set; }
+        public string Varaustilanne { get; set; } = "Vapaa"; // default "Vapaa"
+    }
+
+}
--- Mokkivaraus/Views/MokitPage.xaml.cs
+++ Mokkivaraus/Views/MokitPage.xaml.cs
@@ -1,11 +1,16 @@
-namespace Mokkivaraus.Views;
+﻿namespace Mokkivaraus.Views;
+using System.Collections.ObjectModel;
+
 
 public partial class MokitPage : ContentPage
 {
-	public MokitPage()
+    private ObservableCollection<Mokki> mokkiLista = new ObservableCollection<Mokki>();
+
+    public MokitPage()
 	{
 		InitializeComponent();
-	}
+        MokkiListaView.ItemsSource = mokkiLista;
+    }
 
     //MENU - sidebar////////////////////
 
@@ -52,4 +57,53 @@ public partial class MokitPage : ContentPage
 
         Navigation.RemovePage(this);
     }
+
+    //lisäämökki popuplist
+    private void OnAddCabinClicked(object sender, EventArgs e)
+    {
+        PopupOverlay.IsVisible = true;
+    }
+
+
+    private async void OnSavePopupClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Tallenna", "Mökki lisätty (esimerkki)", "OK");
+        PopupOverlay.IsVisible = false;
+    }
+
+    private void OnSaveCabinClicked(object sender, EventArgs e)
+    {
+        var yeniMokki = new Mokki
+        {
+            Nimi = NimiEntry.Text,
+            Sijainti = SijaintiEntry.Text,
+            Hinta = decimal.TryParse(HintaEntry.Text, out decimal hinta) ? hinta : 0,
+            Kapasiteetti = int.TryParse(KapasiteettiEntry.Text, out int kapasiteetti) ? kapasiteetti : 0,
+            Alue = AlueEntry.Text
+        };
+
+        mokkiLista.Add(yeniMokki);
+        PopupOverlay.IsVisible = false;
+
+        
+        NimiEntry.Text = "";
+        SijaintiEntry.Text = "";
+        HintaEntry.Text = "";
+        KapasiteettiEntry.Text = "";
+        AlueEntry.Text = "";
+    }
+
+    private void OnCancelPopupClicked(object sender, EventArgs e)
+    {
+        PopupOverlay.IsVisible = false;
+    }
+
+    private void OnPoistaClicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var mokki = (Mokki)button.CommandParameter;
+
+        mokkiLista.Remove(mokki);
+    }
+
 }
