97d62edf75388f04d3cb0f589a802d547c8121ba
Miikka Kuronen <s2419842@edu.savonia.fi>
2025-04-14 21:31:41 +0300

    Lisätty AlueetPage

---
+++ Mokkivaraus/Views/AlueetPage.xaml.cs
@@ -0,0 +1,26 @@
+namespace Mokkivaraus.Views
+{
+    public partial class AlueetPage : ContentPage
+    {
+        public AlueetPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAddRegionClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnEditClicked(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void OnSearchRegion(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
