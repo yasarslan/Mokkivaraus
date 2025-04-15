a64c2daaf18224a9908d042b3f68ba68ff800637
IrInAnGeLiN <IrInAnGeLiN@users.noreply.github.com>
2025-04-15 22:32:12 +0300

    db again

--- Mokkivaraus/DatabaseHelper.cs
+++ Mokkivaraus/DatabaseHelper.cs
@@ -10,7 +10,7 @@ namespace Mokkivaraus
 {
     public class DatabaseHelper
     {
-        private readonly string connectionString = "server=127.0.0.1;port=3306;database=vn;user=root;password=12345";
+        private readonly string connectionString = "server=;port=;database=;user=;password=";
             public async Task<DataTable> GetDataAsync(string query)
             {
                         DataTable dt = new DataTable();
